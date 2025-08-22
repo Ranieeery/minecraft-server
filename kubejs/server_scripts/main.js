3bf150bcd8b8234447bf52dc37ce8f5c477e020f
Ranieeery <raniery2003@hotmail.com>
2025-08-22 15:26:46 -0300

    fix: update welcome message for new players with instructions and documentation link

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -2,7 +2,6 @@ ServerEvents.tags('block', event => {
   event.add("ftbchunks:interact_whitelist", "waystones:waystone")
 })
 
-//Welcome
 PlayerEvents.loggedIn(event => { 
-  event.player.tell("testeeeee")
+  event.player.tell("[§6Server]§f Seja bem vindo ao servidor!\n Se esta é a sua primeira vez jogando configure o Voice Mod apertando a tecla §2 \"V\". Para acessar a documentação acesse https://ranieeery.github.io/minecraft-server/")
 })
